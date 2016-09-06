b0d4ec3673e67681d43233264ca5b0e347d394b2
patrickcuauro <patrickcuaur@gmail.com>
2016-09-06 14:38:59 -0430

    arreglos

--- js/frm_agencias.js
+++ js/frm_agencias.js
@@ -142,7 +142,7 @@ function Permisos() {
     //var msjModal
 function save() {
         var record = {};
-        record.hdn_id_contacto = $('#hdn_id_contacto').val();
+        record.id = $('#hdn_id').val();
         record.Rif = $('#Rif').val();
         record.Nombre = $('#Nombre').val();
         record.RazonSocial = $('#RazonSocial').val();
@@ -180,7 +180,7 @@ function save() {
                         "keyboard": "true"
                     }
 
-                    $('#hdn_id_contacto').val(response.hdn_id_contacto);
+                    $('#hdn_id').val(response.hdn_id);
                     msjModal = $.remodal.lookup[$('[data-remodal-id=msjModal]').data('remodal')];
                     msjModal.open();
                     //$('#msjModal').modal(options);
@@ -189,7 +189,7 @@ function save() {
                         $("#msj").html("El record ha sido guardado con errores: " + response.msj);
                     }
                     else {
-                        $('#hdn_id_contacto').val(response.id)
+                        $('#hdn_id').val(response.id)
                         $("#msj").html("El record ha sido guardado con exito.");
                     }
                     //})
